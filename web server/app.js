ca1e41dbb234878125b13144520d353578c36c9e
shivamengineersworkbook <shivamkalra2017@gmail.com>
2018-08-23 02:16:38 +0530

    Added template to web server

--- web server/app.js
+++ web server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/',function(req,res){
   // res.send('<h2>Hello Express</h2>');
   res.send({
@@ -13,4 +15,10 @@ app.get('/',function(req,res){
   });
 });
 
-app.listen(8080);
+app.get('/about',(req,res) => {
+  res.send('<h2>About Page</h2>');
+});
+
+app.listen(3000, () => {
+  console.log('server up on port 3000');
+});
