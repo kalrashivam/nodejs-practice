7de7ece6dd38e5196010e45caee6a9d68bc23557
shivamengineersworkbook <shivamkalra2017@gmail.com>
2018-08-29 23:21:57 +0530

    Added deletion and updation to todo-api

--- todo-api/playground/index.js
+++ todo-api/playground/index.js
@@ -1,19 +1,26 @@
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db) => {
+MongoClient.connect('mongodb://localhost:27017',(err,client) => {
    if(err){
    return console.log('unable to connect to mongo server');
    }
-   console.console.log('Connected to mongoDb server');
+   console.log('Connected to mongoDb server');
 
-   db.collections('Todos').insertOne({
-     text: 'Something that i have to do',
-     status: false},(err, result) => {
-     if(err){
-       return console.log('unable to insert to-do')
-       }
-     console.log(JSON.stringify(result.ops , undefined, 2))
+   const db = client.db('TodoApi');
+   // db.collection('todos').insertOne({
+   //   text: 'have to do',
+   //   status: true},(err, result) => {
+   //   if(err){
+   //     return console.log('unable to insert to-do')
+   //     }
+   //   console.log(JSON.stringify(result.ops , undefined, 2));
+   // });
+
+   db.collection('todos').find({status:false}).toArray().then((docs) => {
+     console.log(JSON.stringify(docs, undefined, 2));
+   }, (error) => {
+     console.log(error);
    });
 
-   db.close();
+   client.close();
 });
